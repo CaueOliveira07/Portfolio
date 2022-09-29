5f0d404e4104d5366a31e04626e969107b062b40
Cauê Oliveira <cauedeoliveiracosta@gmail.com>
2022-09-29 20:01:13 -0300

    menu responsivo criado

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function menuClick() {
+
+    if (document.querySelector('nav ul').style.display == 'none') {
+        document.querySelector('nav ul').style.display = 'flex'
+    } else {
+        document.querySelector('nav ul').style.display = 'none'
+    }
+    
+}
+
+
